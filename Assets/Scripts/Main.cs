5511027492676b3e1c7bf48d785330ccbf7333b4
J0Hyejin <141013135+J0Hyejin@users.noreply.github.com>
2023-10-17 16:26:40 +0900

    하루치 작업
    혜진

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,10 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
+    [SerializeField]
+    AnimationCurve curveMove;
+
+    float appearTIme = 0.5f;
+
+    Vector3 upPoz = new Vector3(0, -110, 0);
+    Vector3 downPoz = new Vector3(0, -1150, 0);
+
+    public GameObject mainPanel;
     public GameObject optionPanel;
     public GameObject howToPanel;
+<<<<<<< Updated upstream
     public GameObject stageSelect;
 
     public void GameStartButton()
@@ -31,9 +42,19 @@ public class Main : MonoBehaviour
     {
         optionPanel.SetActive(false);
     }
+=======
+    public GameObject levelPanel;  
+    public GameObject galleryPanel;
+
+   
+
+>>>>>>> Stashed changes
 
     public void gameExit()
     {
         Application.Quit();
     }
+
+
+
 }
