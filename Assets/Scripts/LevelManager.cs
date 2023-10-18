37d2d432bd033d8d060fc820a29704084104b9b0
iqnrfosem <146904862+iqnrfosem@users.noreply.github.com>
2023-10-18 15:11:18 +0900

    Revert "Merge branch 'JHJ'"
    This reverts commit dabfab6c52e9f89684787f5c4b46f90392bc2bc7, reversing
    changes made to 22e73c96637dc96ae1804f3af0939d66d30f8723.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,7 +1,6 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
@@ -9,79 +8,10 @@ public class LevelManager : MonoBehaviour
     public GameObject img;
     public GameObject leftButton;
     public GameObject rightButton;
-    public GameObject warnPanel;
     public TextMeshProUGUI levelName;
 
-    string[] lvNames = { "Tutorial", "Stage1", "Stage2", "Stage3" };
-
-    int showingLv = 0;
-    int nowLv = 0;
-    int cleardLv = 0;
-
-    private void Start()
+    public void TutoClick()
     {
-        if (PlayerPrefs.HasKey("cleardLv"))
-            cleardLv = PlayerPrefs.GetInt("cleardLv");
-        else
-            cleardLv = 0;
-        Debug.Log("cleardLv" + cleardLv);
-        leftButton.SetActive(false);
-    }
-
-    public void LevelSelect()
-    {
-        switch (nowLv)
-        {
-            case 0:
-                SceneManager.LoadScene("Tuto");
-                break;
-            case 1:
-                SceneManager.LoadScene("Stage1");
-                break;
-            case 2:
-                SceneManager.LoadScene("Stage2");
-                break;
-            case 3:
-                SceneManager.LoadScene("Stage3");
-                break;
-        }
-    }
-
-    public void TempLevelUp()
-    {
-        cleardLv++;
-        PlayerPrefs.SetInt("cleardLv", cleardLv);
-    }
-
-    public void RIghtClick()
-    {
-        if (nowLv != 3)
-            nowLv++;
-        Debug.Log(nowLv);
-    }
-
-    public void LeftClick()
-    {
-        if (nowLv != 0)
-            nowLv--;
-        Debug.Log(nowLv);
-    }
-
-    private void Update()
-    {
-        img.GetComponent<Image>().sprite = levels[nowLv];
-        levelName.text = lvNames[nowLv];
-        if (nowLv == 0)
-            leftButton.SetActive(false);
-        else
-            leftButton.SetActive(true);
-        if (nowLv == 3)
-            rightButton.SetActive(false);
-        else
-            rightButton.SetActive(true);
-        if (cleardLv < nowLv)
-            warnPanel.SetActive(true);
-        else
-            warnPanel.SetActive(false);
+        SceneManager.LoadScene("Tuto");
     }
 }
